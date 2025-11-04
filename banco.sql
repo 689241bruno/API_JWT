d05637e5696d369f4d9f185c8ed5fb77288b0e6f
Bruno S. Santos <689241bruno@gmail.com>
2025-11-04 20:53:02 -0300

    registro com tipo de usuario

--- banco.sql
+++ banco.sql
@@ -4,3 +4,6 @@ id INT AUTO_INCREMENT PRIMARY KEY,
 nome varchar(255) NOT NULL UNIQUE,
 email varchar(255) NOT NULL UNIQUE,
 senha varchar(255) NOT NULL);
+
+ALTER TABLE usuarios
+ADD tipo ENUM('aluno', 'professor', 'admin') NOT NULL DEFAULT 'aluno';
--- index.js
+++ index.js
@@ -22,18 +22,23 @@ app.get("/usuarios", async (req, res) => {
 });
 
 app.post("/usuarios", async (req, res) => {
-  const { nome, email, senha } = req.body;
-  if (!nome || !email || !senha) {
+  const { nome, email, senha, tipo } = req.body;
+  if (!nome || !email || !senha || !tipo) {
     return res.status(400).json({ error: "campos vazios" });
+  }
+  const tiposPermitidos = ["aluno", "professor", "admin"];
+  if (!tiposPermitidos.includes(tipo)) {
+    return res.status(400).json({ error: "Tipo de usuário inválido." });
   }
 
   try {
-    const query = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
-    const [result] = await pool.query(query, [nome, email, senha]);
+    const query =
+      "INSERT INTO usuarios (nome, email, senha, tipo) VALUES (?, ?, ?, ?)";
+    const [result] = await pool.query(query, [nome, email, senha, tipo]);
     res.status(201).json({
       message: "usuario adicionado com sucesso!",
       id: result.insertId,
-      usuario: { nome, email, senha },
+      usuario: { nome, email, senha, tipo },
     });
   } catch (err) {
     console.log("erro ao adicionar usuario", err);
@@ -42,26 +47,40 @@ app.post("/usuarios", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-  const { id, nome, email, senha } = req.body;
+  const { email, senha, lembrar } = req.body;
+  console.log("-> dados de Login", { email, senha, lembrar });
   if (!email || !senha) {
     return res.status(400).json({ error: "campos vazios" });
   }
+
   try {
     const query = "SELECT * FROM usuarios WHERE email=? AND senha=?";
     const [result] = await pool.query(query, [email, senha]);
+
     const user = result[0];
+    if (result.length === 0) {
+      return res.status(401).json({ error: "Email ou senha inválidos." });
+    }
+
     const payload = {
-      id: result.id,
-      email: result.email,
+      id: user.id,
+      email: user.email,
     };
 
+    const expire = lembrar ? "30d" : "7d";
+
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: "1h",
+      expiresIn: expire,
     });
 
-    res
-      .status(201)
-      .json({ id: user.id, nome: user.nome, email: user.email, token: token });
+    res.status(201).json({
+      id: user.id,
+      nome: user.nome,
+      email: user.email,
+      token: token,
+      lembrar: req.body.lembrar,
+      tipo: user.tipo,
+    });
   } catch (err) {
     console.log("erro ao logar usuario", err);
     res.status(500).json({ error: "erro interno" });
